7be9e815150b6699d2779fd8a4359083fe1f3352
RadidDesafandri <radietdesfandri@gmail.com>
2025-01-05 13:53:06 +0700

    feat: added animation

--- src/app/components/Invitations.tsx
+++ src/app/components/Invitations.tsx
@@ -10,6 +10,7 @@ import AgendaInvitation from "./AgendaInvitation";
 import GuestInvitation from "./GuestInvitation";
 import GiftInvitations from "./GiftInvitations";
 import Footer from "@/components/Footer";
+import AnimatedOpacity from "@/components/Motions/AnimatedOpacity";
 
 interface InvitationsProps {
   isOpen: boolean;
@@ -41,77 +42,107 @@ const Invitations: React.FC<InvitationsProps> = ({
         height={100}
         className="absolute -right-3 -top-3 w-32 opacity-60"
       />
-      <div className="absolute left-0 right-0 top-0 flex h-full w-full flex-col items-center gap-y-[1px]">
-        <Image
-          src={"/pic1.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic2.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic3.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic4.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic5.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic6.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pengantin3.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic1.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic2.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
-        <Image
-          src={"/pic3.jpg"}
-          alt="Picpengantin"
-          width={500}
-          height={500}
-          className="-z-0 h-full w-full object-cover opacity-30"
-        />
+      <div className="absolute left-0 right-0 top-0 flex h-full w-full flex-col items-center">
+        <AnimatedOpacity duration={2}>
+          <Image
+            src={"/pic1.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic2.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic3.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic4.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic5.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic6.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pengantin3.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic1.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic2.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic3.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full w-full object-cover opacity-30"
+          />
+        </AnimatedOpacity>
+        <AnimatedOpacity duration={2} threshold={0}>
+          <Image
+            src={"/pic7.jpg"}
+            alt="Picpengantin"
+            width={500}
+            height={500}
+            className="-z-0 h-full max-h-72 w-full object-cover object-top opacity-30"
+          />
+        </AnimatedOpacity>
       </div>
 
       <BottomBar />
